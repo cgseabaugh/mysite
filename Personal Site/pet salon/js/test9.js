2c70bc579325b372e04f8051c1518f6754e4a5bd
cgseabaugh <56783671+cgseabaugh@users.noreply.github.com>
2020-11-09 09:13:38 -0800

    Add files via upload

---
+++ Personal Site/pet salon/js/test9.js
@@ -0,0 +1,29 @@
+
+$(function(){
+    'use strict'
+    // $('img#logo').on('click', function(){
+    //     $('main').load("../register.html");
+    // });
+
+    $('main').on('click',loadAjax);
+
+    function loadAjax(){
+        $.ajax("js/pets.txt",{
+            success:addContent,
+            type:"GET",
+            dataType: 'text'
+        });
+    }
+    function addContent(data,status,extra){
+        $('div#data').text(data);
+        console.log(status);
+    }
+    
+});
+
+// delete header, nav, and footer from register, services, and abotu
+//add main tag to index
+// create app.js, add click events and load functions
+// load register, services, and about on the index when nav is clicked
+// add #id's to all navs
+
